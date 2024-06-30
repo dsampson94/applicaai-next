a0232649c185b4a6f1b22a2fd6e2216edbba6177
dsampson94 <davesampson15@gmail.com>
2024-06-30 12:15:12 +0200

    db connect

--- lib/db/connect.ts
+++ lib/db/connect.ts
@@ -1,9 +1,15 @@
 import clientPromise from "./mongodb";
 
+const dbName = process.env.MONGODB_DB;
+
+if (!dbName) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_DB"');
+}
+
 async function connectToDatabase() {
     const client = await clientPromise;
-    const db = client.db();
-    return {client, db};
+    const db = client.db(dbName);
+    return { client, db };
 }
 
 export default connectToDatabase;
